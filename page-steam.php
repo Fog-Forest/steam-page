e388ddb2f705b150453173c0c81dff13e0b1ddb6
雾时之森 <ye8980@qq.com>
2020-04-02 02:27:06 +0800

    Steam展示页本地版orAPI缓存版

--- page-steam.php
+++ page-steam.php
@@ -12,12 +12,24 @@ get_header(); ?>
 </style>
 <?php
 	$steamid = "76561198849944519";//你的SteamID，可以在这里获取https://steamsignature.com/
+	$steamAPI = "";//Steam自建的API，没有就别填了用本地手动获取JSON更新 json/steam.json 文件内容
 	echo "<a href=\"https://steamcommunity.com/profiles/".$steamid."\" target=\"_blank\"><img src=\"https://steamsignature.com/profile/schinese/".$steamid.".png\"></a>";
 	//上面这个图片是实时更新的
-	$data = json_decode(file_get_contents("json/steam.json"),true);//获取本地JSON并转数组（社区被墙折中办法）
-	//$data = json_decode(file_get_contents("API地址"),true);//使用API获取JSON并转数组（我的API稳定性不保证）
+
+	$data = json_decode(file_get_contents("json/steam.json"), true);//获取本地JSON并转数组（社区被墙折中办法）
+
+	//以下为API获取数据，支持缓存，使用此记得注释掉上面的一行
+/*	if((time() - filemtime ("json/steam.json")) > 86400){//缓存24小时
+		file_put_contents("json/steam.json", file_get_contents($steamAPI));
+		$data = file_get_contents("json/steam.json");
+	} else {
+		$data = file_get_contents("json/steam.json");
+	}
+	$data = json_decode($data, true);
+*/	
 	//echo '<pre>';
 	//echo $data;//我瞅瞅里面有啥玩意儿
+	
 	$count = count($data); //游戏数量统计
 	if($count == 0){
 		echo "<p>哦噢~你好像并没有够买任何游戏哦！</p>";
@@ -31,13 +43,16 @@ get_header(); ?>
 			if($i > 8){//首次要展示游戏数目默认为8个
 				$num = "more";
 			}
+			if($data[$i]['appid'] == 205790){
+				continue;//Dota2 Test图片有问题，跳过该游戏
+			}
 			echo "<div class=\"steam-game-item ".$num."\"><div class=\"steam-game-picture\"><img src=\"".$data[$i]['logo']."\" referrerpolicy=\"no-referrer\"></div><div class=\"steam-game-info\"><div class=\"steam-game-title\"><a target=\"_blank\" href=\"https://store.steampowered.com/app/".$data[$i]['appid']."\">".$data[$i]['name']."</a></div><div class=\"steam-game-meta\"><span class=\"steam-game-info-time\">总时数 ".$hours." 小时</span></div></div><div class=\"steam-game-link\"><a class=\"steam-game-button\" target=\"_blank\" href=\"https://store.steampowered.com/app/".$data[$i]['appid']."\">商店页面</a><a class=\"steam-game-button\" target=\"_blank\" href=\"https://steamcommunity.com/app/".$data[$i]['appid']."/discussions\">论坛</a><a class=\"steam-game-button\" target=\"_blank\" href=\"https://steamcommunity.com/search/groups/?text=".$data[$i]['name']."\">查找社区组</a><a class=\"steam-game-button\" target=\"_blank\" href=\"https://store.steampowered.com/news/?appids=".$data[$i]['appid']."\">相关新闻</a><a class=\"steam-game-button\" target=\"_blank\" href=\"https://steamdb.info/app/".$data[$i]['appid']."\">SteamDB</a></div></div>";
 		}
 	}
 ?>
 <center><div class="showall">. Show All .</div></center><br>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery@3.2.1/dist/jquery.min.js"></script>
-<script type="text/javascript">//收缩展示=.=
+<script type="text/javascript">//收缩展示
 $(document).ready(function(){
 	$(".more").hide();
 	$(".showall").click(function(){
