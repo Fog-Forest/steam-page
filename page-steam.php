c179a683d8e82bec01c0c6532321bc2c8a07829e
雾时之森 <ye8980@qq.com>
2020-04-01 23:13:58 +0800

    增加硬核分页

--- page-steam.php
+++ page-steam.php
@@ -2,19 +2,20 @@
 
 /**
  Template Name: Steam游戏库
+ Template Author：老蘑菇
  */
 
 get_header(); ?>
 
 <style>
-.steam-game-tabs{margin-bottom:15px;margin-top:15px;}.steam-game-tab{padding:5px;}a.steam-game-tab{text-decoration:none;}.steam-game-active{background:#657b83;color:#fff;}.steam-game-item{padding-bottom:10px;position:relative;clear:both;min-height:100px;padding:10px 0;border-bottom:1px #ddd solid;}@media screen and (max-width:600px){.steam-game-item{width:100%;}}.steam-game-picture{position:absolute;left:0;top:10px;width:184px;padding-top:5px;}.steam-game-info{margin:25px 0;padding-left:194px;}.steam-game-meta{font-size:12px;padding-right:10px;}.steam-game-pagination{margin-top:15px;text-align:center;margin-bottom:10px;}.steam-game-button{padding:5px;text-decoration:none;display:inline-block;}.steam-game-button:hover{background:#657b83;color:#fff;}.steam-game-hide{display:none;}.steam-game-show{display:block;}.steam-game-title{font-size:18px;}.steam-game-title a{line-height:1;color:#99a9bf;}.steam-game-link{min-height:50px;}.steam-game-link a{color:#99a9bf;}@media (max-width:400px){.steam-game-picture{padding-left:5px;}.steam-game-info{margin:10px 0;padding:85px 0 0 5px;}}
+.steam-game-tabs{margin-bottom:15px;margin-top:15px;}.steam-game-tab{padding:5px;}a.steam-game-tab{text-decoration:none;}.steam-game-active{background:#657b83;color:#fff;}.steam-game-item{padding-bottom:10px;position:relative;clear:both;min-height:100px;padding:10px 0;border-bottom:1px #ddd solid;}@media screen and (max-width:600px){.steam-game-item{width:100%;}}.steam-game-picture{position:absolute;left:0;top:10px;width:184px;padding-top:5px;}.steam-game-info{margin:25px 0;padding-left:194px;}.steam-game-meta{font-size:12px;padding-right:10px;}.steam-game-pagination{margin-top:15px;text-align:center;margin-bottom:10px;}.steam-game-button{padding:5px;text-decoration:none;display:inline-block;}.steam-game-button:hover{background:#657b83;color:#fff;}.steam-game-hide{display:none;}.steam-game-show{display:block;}.steam-game-title{font-size:18px;}.steam-game-title a{line-height:1;color:#99a9bf;}.steam-game-link{min-height:50px;}.steam-game-link a{color:#99a9bf;}@media (max-width:400px){.steam-game-picture{padding-left:5px;}.steam-game-info{margin:10px 0;padding:85px 0 0 5px;}}.showall{font-size:20px;color:orange;padding:20px 0 20px 0;}.showall:hover{color:#e67474;}
 </style>
-
 <?php
 	$steamid = "76561198849944519";//你的SteamID，可以在这里获取https://steamsignature.com/
 	echo "<a href=\"https://steamcommunity.com/profiles/".$steamid."\" target=\"_blank\"><img src=\"https://steamsignature.com/profile/schinese/".$steamid.".png\"></a>";
 	//上面这个图片是实时更新的
-	$data = json_decode(file_get_contents("gamelist/steam.json"),true);//获取本地JSON并转数组（社区被墙我也没辙）
+	$data = json_decode(file_get_contents("json/steam.json"),true);//获取本地JSON并转数组（社区被墙折中办法）
+	//$data = json_decode(file_get_contents("json/steam.json"),true);//使用我的API获取JSON并转数组（我的API稳定性不保证）
 	//echo '<pre>';
 	//echo $data;//我瞅瞅里面有啥玩意儿
 	$count = count($data); //游戏数量统计
@@ -27,10 +28,24 @@ get_header(); ?>
 			} else {
 				$hours = $data[$i]['hours_forever'];
 			}
-			echo "<div class=\"steam-game-item\"><div class=\"steam-game-picture\"><img src=\"".$data[$i]['logo']."\" referrerpolicy=\"no-referrer\"></div><div class=\"steam-game-info\"><div class=\"steam-game-title\"><a target=\"_blank\" href=\"https://store.steampowered.com/app/".$data[$i]['appid']."\">".$data[$i]['name']."</a></div><div class=\"steam-game-meta\"><span class=\"steam-game-info-time\">总时数 ".$hours." 小时</span></div></div><div class=\"steam-game-link\"><a class=\"steam-game-button\" target=\"_blank\" href=\"https://store.steampowered.com/app/".$data[$i]['appid']."\">商店页面</a><a class=\"steam-game-button\" target=\"_blank\" href=\"https://steamcommunity.com/app/".$data[$i]['appid']."/discussions\">论坛</a><a class=\"steam-game-button\" target=\"_blank\" href=\"https://steamcommunity.com/search/groups/?text=".$data[$i]['name']."\">查找社区组</a><a class=\"steam-game-button\" target=\"_blank\" href=\"https://store.steampowered.com/news/?appids=".$data[$i]['appid']."\">相关新闻</a><a class=\"steam-game-button\" target=\"_blank\" href=\"https://steamdb.info/app/".$data[$i]['appid']."\">SteamDB</a></div></div>";
+			if($i < 8){//首次展示数目
+				$num = "";
+			} else {
+				$num = "more";
+			}
+			echo "<div class=\"steam-game-item ".$num."\"><div class=\"steam-game-picture\"><img src=\"".$data[$i]['logo']."\" referrerpolicy=\"no-referrer\"></div><div class=\"steam-game-info\"><div class=\"steam-game-title\"><a target=\"_blank\" href=\"https://store.steampowered.com/app/".$data[$i]['appid']."\">".$data[$i]['name']."</a></div><div class=\"steam-game-meta\"><span class=\"steam-game-info-time\">总时数 ".$hours." 小时</span></div></div><div class=\"steam-game-link\"><a class=\"steam-game-button\" target=\"_blank\" href=\"https://store.steampowered.com/app/".$data[$i]['appid']."\">商店页面</a><a class=\"steam-game-button\" target=\"_blank\" href=\"https://steamcommunity.com/app/".$data[$i]['appid']."/discussions\">论坛</a><a class=\"steam-game-button\" target=\"_blank\" href=\"https://steamcommunity.com/search/groups/?text=".$data[$i]['name']."\">查找社区组</a><a class=\"steam-game-button\" target=\"_blank\" href=\"https://store.steampowered.com/news/?appids=".$data[$i]['appid']."\">相关新闻</a><a class=\"steam-game-button\" target=\"_blank\" href=\"https://steamdb.info/app/".$data[$i]['appid']."\">SteamDB</a></div></div>";
 		}
 	}
 ?>
-<br><br><br>
+<center><div class="showall">. Show All .</div></center><br>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery@3.2.1/dist/jquery.min.js"></script>
+<script type="text/javascript">//硬核分页展示=.=
+$(document).ready(function(){
+	$(".more").hide();
+	$(".showall").click(function(){
+		$(".more").show(1000);
+		$(".showall").hide();
+	});
+});</script>
 
 <?php get_footer();
