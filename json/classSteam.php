b29c3b41cbdd59e27a741ce8a8f4aee0f719b33a
雾时之森 <ye8980@qq.com>
2020-05-04 21:30:11 +0800

    更新

--- json/classSteam.php
+++ json/classSteam.php
@@ -5,30 +5,59 @@ class Steamgame
     public $game_logo = array();  // 游戏图片
     public $game_appid = array();  // 游戏Appid
     public $game_hours = array();  // 游戏时长
+    private $game_data = array();
 
     // 主函数
-    public function __construct($api_url)
+    public function __construct($id, $steamAPI, $type)
     {
-         // 缓存Steam API数据
-        if((time() - filemtime("json/steam.json")) > 86400)  // 缓存24小时
+        // 个人信息卡片
+        if($type == 1)
         {
-		    file_put_contents("json/steam.json", file_get_contents($api_url));
-		    $game_info = file_get_contents("json/steam.json");
-	    } else {
-		    $game_info = file_get_contents("json/steam.json");
+            echo "<a href=\"https://steamcommunity.com/profiles/".$id."\" target=\"_blank\"><img src=\"https://steamsignature.com/status/schinese/".$id.".png\"></a>";
+        }
+        elseif ($type == 2)
+        {
+            echo "<a href=\"https://steamcommunity.com/profiles/".$id."\" target=\"_blank\"><img src=\"https://steamsignature.com/card/0/".$id.".png\"></a>";
+        }
+        else 
+        {
+            echo "<a href=\"https://steamcommunity.com/profiles/".$id."\" target=\"_blank\"><img src=\"https://steamsignature.com/profile/schinese/".$id.".png\"></a>";
+        }
+
+        // 判断走本地API 或 走外链API
+        if($steamAPI){
+	        $api_url = $steamAPI."?id=".$id."&type=all";  // 拼合外链api_url
+            if((time() - filemtime("json/steam.json")) > 86400)  // 缓存Steam API数据24小时，使用我的API请不要改为0
+            {
+                file_put_contents("json/steam.json", file_get_contents($api_url));
+                $game_info = file_get_contents("json/steam.json");
+            } else {
+                $game_info = file_get_contents("json/steam.json");
+            }
+        }
+        else
+        {
+            $api_url = "json/SteamAPI.php?id=".$id."&type=all";  // 拼合本地api_url
+            if((time() - filemtime("json/steam.json")) > 86400)  // 缓存Steam API数据24小时
+            {
+                file_put_contents("json/steam.json", file_get_contents($api_url));
+                $game_info = file_get_contents("json/steam.json");
+            } else {
+                $game_info = file_get_contents("json/steam.json");
+            }
         }
         
-        $temp = json_decode($game_info, true);
+        $this->game_data = json_decode($game_info, true);
         //echo "<pre>";
-        //echo var_dump($temp);
+        //echo var_dump($this->game_data);
 
         // 存储获得的数据,关联数组
-        for($i=0; $i<count($temp); $i++)
+        for($i=0; $i<count($this->game_data); $i++)
         {
-            array_push($this->game_name, $temp[$i]['name']);
-            array_push($this->game_logo, $temp[$i]['logo']);
-            array_push($this->game_appid, $temp[$i]['appid']);
-            array_push($this->game_hours, !empty($temp[$i]['hours_forever'])?$temp[$i]['hours_forever']:"0");
+            array_push($this->game_name, $this->game_data[$i]['name']);
+            array_push($this->game_logo, $this->game_data[$i]['logo']);
+            array_push($this->game_appid, $this->game_data[$i]['appid']);
+            array_push($this->game_hours, !empty($this->game_data[$i]['hours_forever'])?$this->game_data[$i]['hours_forever']:"0");
         }
     }
 }
--- page-steam.php
+++ page-steam.php
@@ -11,24 +11,21 @@ get_header(); ?>
 .steam-game-tabs{margin-bottom:15px;margin-top:15px;}.steam-game-tab{padding:5px;}a.steam-game-tab{text-decoration:none;}.steam-game-active{background:#657b83;color:#fff;}.steam-game-item{padding-bottom:10px;position:relative;clear:both;min-height:100px;padding:10px 0;border-bottom:1px #ddd solid;}@media screen and (max-width:600px){.steam-game-item{width:100%;}}.steam-game-picture{position:absolute;left:0;top:10px;width:184px;padding-top:5px;}.steam-game-info{margin:25px 0;padding-left:194px;}.steam-game-meta{font-size:12px;padding-right:10px;}.steam-game-pagination{margin-top:15px;text-align:center;margin-bottom:10px;}.steam-game-button{padding:5px;text-decoration:none;display:inline-block;}.steam-game-button:hover{background:#657b83;color:#fff;}.steam-game-hide{display:none;}.steam-game-show{display:block;}.steam-game-title{font-size:18px;}.steam-game-title a{line-height:1;color:#99a9bf;}.steam-game-link{min-height:50px;}.steam-game-link a{color:#99a9bf;}@media (max-width:400px){.steam-game-picture{padding-left:5px;}.steam-game-info{margin:10px 0;padding:85px 0 0 5px;}}.showall{font-size:20px;color:orange;padding:20px 0 20px 0;}.showall:hover{color:#e67474;}
 </style>
 <?php
-	$steamid = "76561198849944519";  // 你的SteamID，可以在这里获取https://steamsignature.com/
-	$steamAPI = "https://api.mushroom.ga/SteamAPI.php";  // SteamAPI，我提供了两个API，更推荐你自建
+	$id = "76561198849944519";  // 你的SteamID，可以在这里获取https://steamsignature.com/
+	$steamAPI = "https://api.mushroom.ga/SteamAPI.php";  // SteamAPI，我提供了两个API，更推荐你自建。留空走本地（需境外服务器）。
+	$page = 8;  // 首次要展示游戏数目默认为8个
 
-	// PS: 下面这个图片是实时更新的，有三种样式，用哪种就注释掉其他两个
-	echo "<a href=\"https://steamcommunity.com/profiles/".$steamid."\" target=\"_blank\"><img src=\"https://steamsignature.com/profile/schinese/".$steamid.".png\"></a>";  // Profile
-	// echo "<a href=\"https://steamcommunity.com/profiles/".$steamid."\" target=\"_blank\"><img src=\"https://steamsignature.com/status/schinese/".$steamid.".png\"></a>";  // Lite Status
-	// echo "<a href=\"https://steamcommunity.com/profiles/".$steamid."\" target=\"_blank\"><img src=\"https://steamsignature.com/card/0/".$steamid.".png\"></a>";  // Card
-	
-	// API_url 拼合
-	$api_url = $steamAPI."?id=".$steamid."&type=all";
 	require_once("json/classSteam.php");
-	$steam = new Steamgame($api_url);
+	$steam = new Steamgame($id, $steamAPI, 3);  // PS: 个人信息图片是实时更新的，有三种样式，默认为Profile，1为"Lite Status"，2为"Card"
 	$count = count($steam->game_name);  // 游戏数量统计
 	if($count == 0){
 		echo "<p>哦噢~你好像并没有够买任何游戏哦！</p>";
-	} else {
-		for ($i = 0; $i < $count; $i++) {
-			if($i > 8){  // 首次要展示游戏数目默认为8个
+	}
+	else
+	{
+		for ($i = 0; $i < $count; $i++)
+		{
+			if($i > $page){
 				$num = "more";
 			}
 			if($steam->game_appid[$i] == 205790){
@@ -36,9 +33,11 @@ get_header(); ?>
 			}
 			echo "<div class=\"steam-game-item ".$num."\"><div class=\"steam-game-picture\"><img src=\"".$steam->game_logo[$i]."\" referrerpolicy=\"no-referrer\"></div><div class=\"steam-game-info\"><div class=\"steam-game-title\"><a target=\"_blank\" href=\"https://store.steampowered.com/app/".$steam->game_appid[$i]."\">".$steam->game_name[$i]."</a></div><div class=\"steam-game-meta\"><span class=\"steam-game-info-time\">总时数 ".$steam->game_hours[$i]." 小时</span></div></div><div class=\"steam-game-link\"><a class=\"steam-game-button\" target=\"_blank\" href=\"https://store.steampowered.com/app/".$steam->game_appid[$i]."\">商店页面</a><a class=\"steam-game-button\" target=\"_blank\" href=\"https://steamcommunity.com/app/".$steam->game_appid[$i]."/discussions\">论坛</a><a class=\"steam-game-button\" target=\"_blank\" href=\"https://steamcommunity.com/search/groups/?text=".$steam->game_name[$i]."\">查找社区组</a><a class=\"steam-game-button\" target=\"_blank\" href=\"https://store.steampowered.com/news/?appids=".$steam->game_appid[$i]."\">相关新闻</a><a class=\"steam-game-button\" target=\"_blank\" href=\"https://steamdb.info/app/".$steam->game_appid[$i]."\">SteamDB</a></div></div>";
 		}
+		if ($conut > $page){
+			echo "<center><div class=\"showall\">. Show All .</div></center><br>";
+		}
 	}
 ?>
-<center><div class="showall">. Show All .</div></center><br>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery@3.5.0/dist/jquery.min.js"></script>
 <script type="text/javascript">  // 收缩展示
 $(document).ready(function(){
