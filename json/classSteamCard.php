1bae60c6198e9f1510bc778bdede936d0fed4923
MoGuJun <ye8980@gmail.com>
2020-05-28 02:08:01 +0800

    支持分页以及懒加载

--- json/classSteamCard.php
+++ json/classSteamCard.php
@@ -6,11 +6,11 @@ class SteamCard
     {
         // 个人信息卡片
         if ($type == 1) {
-            echo "<div class=\"steam-card-img\"><a href=\"https://steamcommunity.com/profiles/" . $id . "\" target=\"_blank\"><img class=\"lazy\" src=\"https://cdn.jsdelivr.net/gh/Fog-Forest/Picture-Bed/other/loading.svg\" data-src=\"https://steamsignature.com/status/schinese/" . $id . ".png\"></a></div>";
+            echo "<div class=\"steam-card-img\"><a href=\"https://steamcommunity.com/profiles/" . $id . "\" target=\"_blank\"><img class=\"lazy\" src=\"https://cdn.jsdelivr.net/gh/Fog-Forest/Steam-page@1.2/json/loading.svg\" data-src=\"https://steamsignature.com/status/schinese/" . $id . ".png\"></a></div>";
         } elseif ($type == 2) {
-            echo "<div class=\"steam-card-img\"><a href=\"https://steamcommunity.com/profiles/" . $id . "\" target=\"_blank\"><img class=\"lazy\" src=\"https://cdn.jsdelivr.net/gh/Fog-Forest/Picture-Bed/other/loading.svg\" data-src=\"https://steamsignature.com/card/0/" . $id . ".png\"></a></div>";
+            echo "<div class=\"steam-card-img\"><a href=\"https://steamcommunity.com/profiles/" . $id . "\" target=\"_blank\"><img class=\"lazy\" src=\"https://cdn.jsdelivr.net/gh/Fog-Forest/Steam-page@1.2/json/loading.svg\" data-src=\"https://steamsignature.com/card/0/" . $id . ".png\"></a></div>";
         } else {
-            echo "<div class=\"steam-card-img\"><a href=\"https://steamcommunity.com/profiles/" . $id . "\" target=\"_blank\"><img class=\"lazy\" src=\"https://cdn.jsdelivr.net/gh/Fog-Forest/Picture-Bed/other/loading.svg\" data-src=\"https://steamsignature.com/profile/schinese/" . $id . ".png\"></a></div>";
+            echo "<div class=\"steam-card-img\"><a href=\"https://steamcommunity.com/profiles/" . $id . "\" target=\"_blank\"><img class=\"lazy\" src=\"https://cdn.jsdelivr.net/gh/Fog-Forest/Steam-page@1.2/json/loading.svg\" data-src=\"https://steamsignature.com/profile/schinese/" . $id . ".png\"></a></div>";
         }
         echo "<div class=\"colorline\"></div>";
 
