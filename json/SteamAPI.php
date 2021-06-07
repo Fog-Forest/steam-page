54e531ea1049f23ec6dabee6f742b06ed767e58e
Kinoko <ye8980@gmail.com>
2021-06-07 19:27:08 +0800

    fix:修复类型判断问题，增加完美通关类型，一丢丢的优化

--- json/SteamAPI.php
+++ json/SteamAPI.php
@@ -1,36 +1,46 @@
 <?php
-$steamid = $_GET['id'];  // SteamID，可以在这里获取https://steamsignature.com/
-$showtype = $_GET['type'];  // 默认为最近游玩,所有游戏参数为"all"
+$id = $_GET['id'];  //SteamID，可以在这里获取:https://steamsignature.com
+$type = $_GET['type'];  //all(默认)，recent，perfect
 
-if (empty($steamid)) {
-	die("Steam id 为必须参数！");
+if ($id == NULL) {
+	exit("参数有误！");
 } else {
-	preg_match("#(?<=var\srgGames\s=\s).*?false}}](?=;)#", steaminfo($steamid, $showtype), $json);  // 正则取Steam游戏库JSON
-	echo $json[0];
+	$data = steaminfo($id, $type);
+	preg_match("#(?<=rgGames\s=\s).*?](?=;)#", $data, $json);  //正则取Steam游戏库JSON
+	echo ($json[0]);
 }
 
-// 获取Steam信息函数
-function steaminfo($steamid, $showtype)
+//获取Steam信息(需要设置Steam游戏库为公开)
+function steaminfo($id, $type)
 {
-	if (empty($showtype)) {
-		$url = "https://steamcommunity.com/profiles/$steamid/games/?tab=all";
-	} elseif ($showtype == "all" || $showtype == "recent") {
-		$url = "https://steamcommunity.com/profiles/$steamid/games/?tab=$showtype";
+	if ($type == "recent") {
+		$url = "https://steamcommunity.com/profiles/$id/games/?tab=recent";
+	} elseif ($type == "perfect") {
+		$url = "https://steamcommunity.com/profiles/$id/games/?tab=perfect";
 	} else {
-		$url = "https://steamcommunity.com/profiles/$steamid/games/?tab=all";
+		$url = "https://steamcommunity.com/profiles/$id/games/?tab=all";
 	}
-	$ch = curl_init(); // 初始化curl模块
-	curl_setopt($ch, CURLOPT_URL, $url);  // 登录提交的地址
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  // 以文件流的方式返回不直接输出到页面
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);  // 自动获取新url内容：302跳转
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-		// 发送请求头（返回中文页面）
-		"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36",
-		"Accept-Language: zh-CN,zh"
+
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+		CURLOPT_URL => $url,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => '',
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => 'GET',
+		CURLOPT_HTTPHEADER => array(
+			'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0',
+			'Accept-Language: zh-CN'
+		),
 	));
-	$gameinfo = curl_exec($ch);
-	curl_close($ch);
-	$temp = str_replace("cdn.cloudflare.steamstatic.com", "media.st.dl.pinyuncloud.com", $gameinfo);  // 替换为Steam 国内CDN节点
-	$gameimg = ["https:\/\/media.st.dl.pinyuncloud.com\/steam\/apps\/205790\/capsule_184x69.jpg"];  // 404的图片，例如 Dota2 Test，如果还有自行往里加
-	return str_replace($gameimg, "https://cdn.jsdelivr.net/gh/IMGRU/IMG/2020/05/28/5ece9990c9be7.jpg", $temp);
+	$response = curl_exec($curl);
+	curl_close($curl);
+
+	$error_img = ["https:\/\/steamcdn-a.akamaihd.net\/steam\/apps\/205790\/capsule_184x69.jpg"];  //404的图片，例如："Dota2 Test"，自行增加
+	$response = str_replace("steamcdn-a.akamaihd.net", "media.st.dl.pinyuncloud.com", $response);  //替换图片为大陆节点
+	$info =  str_replace($error_img, "https://cdn.jsdelivr.net/gh/IMGRU/IMG/2020/05/28/5ece9990c9be7.jpg", $response);
+	return $info;
 }
